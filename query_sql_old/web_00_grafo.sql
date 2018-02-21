78b6969844a0dd9d0b3cad67aac3074952e3ea32
leleferrara <33225900+leleferrara@users.noreply.github.com>
2018-02-21 10:24:19 +0100

    sostituito MODO -> ROUTE_TYPE

--- query_sql_old/web_00_grafo.sql
+++ query_sql_old/web_00_grafo.sql
@@ -4,20 +4,20 @@
 CREATE TABLE adb.t_nodi AS (
 
 WITH grafo AS (
-  SELECT id, version, user_id, tstamp, changeset_id, tags, nodes, bbox, linestring as geom, modo, varcosto
+  SELECT id, version, user_id, tstamp, changeset_id, tags, nodes, bbox, linestring as geom, route_type, varcosto
   FROM adb.web_000_grafo_costi 
 )
 
 SELECT
-      id, node_id, dist, pos, geom_line, verifica ,modo,  varcosto,
-     ROW_NUMBER() OVER(PARTITION BY id,modo ORDER BY dist) AS rownum
+      id, node_id, dist, pos, geom_line, verifica ,route_type,  varcosto,
+     ROW_NUMBER() OVER(PARTITION BY id,route_type ORDER BY dist) AS rownum
   FROM 
   (
     SELECT
       a.id, b.node_id, b.sequence_id, 
       (ST_Length(a.geom)*ST_LineLocatePoint(ST_GeometryN(a.geom,1), c.geom)) AS dist, 
       (ST_Length(a.geom)*ST_LineLocatePoint(ST_GeometryN(a.geom,1), c.geom))/ST_LENGTH(a.geom) AS pos, 
-      a.geom AS geom_line, c.geom AS geom_point,a.modo,  a.varcosto,
+      a.geom AS geom_line, c.geom AS geom_point,a.route_type,  a.varcosto,
       CASE WHEN sequence_id>0 AND (ST_Length(a.geom)*ST_LineLocatePoint(ST_GeometryN(a.geom,1), c.geom))=0 THEN 1 ELSE 0 END AS verifica
 
     FROM
@@ -32,7 +32,7 @@ SELECT
   )a
   WHERE verifica = 0 
   GROUP BY
-    id, node_id, dist, pos,geom_line, verifica, modo, varcosto) ;
+    id, node_id, dist, pos,geom_line, verifica, route_type, varcosto) ;
 ------------------------------------------------    
 
 CREATE INDEX t_nodi_idx ON adb.t_nodi USING GIST (geom_line);
@@ -42,7 +42,7 @@ CREATE INDEX t_nodi_idx ON adb.t_nodi USING GIST (geom_line);
 CREATE TABLE adb.t_nodi_fine AS (
 
   SELECT 
-    a.id, a.node_id, a.dist,a.modo, a.varcosto,
+    a.id, a.node_id, a.dist,a.route_type, a.varcosto,
     CASE 
       WHEN b.id IS NULL THEN a.pos
       WHEN (b.id>0 AND a.pos=0) THEN 1
@@ -64,10 +64,10 @@ CREATE TABLE adb.t_linea_chiusa AS
 (SELECT 
     a.id,
     CASE WHEN a.pos=b.pos THEN 0 ELSE 1 END AS linea_chiusa, 
-    a.rownum AS rownum_max, a.modo, a.varcosto
+    a.rownum AS rownum_max, a.route_type, a.varcosto
   FROM 
-    (SELECT  id, MAX(pos)pos, MAX(rownum) rownum, modo, varcosto  FROM adb.t_nodi GROUP BY  id,modo, varcosto) a, 
-    (SELECT id, MAX(pos)pos, MAX(rownum) rownum, modo FROM adb.t_nodi_fine GROUP BY modo,  id) b
+    (SELECT  id, MAX(pos)pos, MAX(rownum) rownum, route_type, varcosto  FROM adb.t_nodi GROUP BY  id,route_type, varcosto) a, 
+    (SELECT id, MAX(pos)pos, MAX(rownum) rownum, route_type FROM adb.t_nodi_fine GROUP BY route_type,  id) b
   WHERE
     a.id=b.id AND a.rownum=b.rownum);
 
@@ -78,33 +78,33 @@ CREATE TABLE adb.t_linea_chiusa AS
 CREATE TABLE adb.t_nodi_all AS 
 (
   -- nodi_fine
-  SELECT  id, node_id, dist, pos, rownum, geom_line, modo, varcosto
+  SELECT  id, node_id, dist, pos, rownum, geom_line, route_type, varcosto
   FROM adb.t_nodi
 
   UNION 
   
   -- linea chiusa: aggiungi un nodo. Il nodo con'pos=0' diventa anche il nodo con 'pos=1'
   SELECT
-     a.id, a.node_id, a.dist, 1 AS pos, (rownum_max+1) AS rownum, geom_line, a.modo, a.varcosto
+     a.id, a.node_id, a.dist, 1 AS pos, (rownum_max+1) AS rownum, geom_line, a.route_type, a.varcosto
   FROM
     adb.t_nodi_fine a,
     adb.t_linea_chiusa b
   WHERE
-    a.id=b.id and a.modo = b.modo AND linea_chiusa=1 AND pos=0
+    a.id=b.id and a.route_type = b.route_type AND linea_chiusa=1 AND pos=0
 );
 CREATE INDEX t_nodi_all_idx ON adb.t_nodi_all USING GIST (geom_line);
 
 -- 5. SPEZZA GLI ARCHI DOVE CI SONO I NODI
 CREATE TABLE adb.t_spezza_archi AS (
   SELECT 
-    ROW_NUMBER() OVER(ORDER BY a.id, a.modo) AS id, a.id AS way_originale, a.node_id AS node_start, b.node_id AS node_end, a.rownum as rownum1, b.rownum as rownum2,
-    ST_LineSubstring(a.geom_line, a.pos, b.pos) AS geom   ,a.modo, a.varcosto
+    ROW_NUMBER() OVER(ORDER BY a.id, a.route_type) AS id, a.id AS way_originale, a.node_id AS node_start, b.node_id AS node_end, a.rownum as rownum1, b.rownum as rownum2,
+    ST_LineSubstring(a.geom_line, a.pos, b.pos) AS geom   ,a.route_type, a.varcosto
   FROM 
     adb.t_nodi_all a, adb.t_nodi_all b 
   WHERE 
     a.id=b.id AND
     a.rownum=(b.rownum-1) AND 
-    a.modo = b.modo 
+    a.route_type = b.route_type 
 );
 
 CREATE INDEX t_spezza_archi_idx ON adb.t_spezza_archi USING GIST (geom);
@@ -113,13 +113,13 @@ CREATE INDEX t_spezza_archi_idx ON adb.t_spezza_archi USING GIST (geom);
 DROP TABLE adb.t_bus_oneway ;
 CREATE TABLE adb.t_bus_oneway AS ( 
 WITH grafo AS (
-  SELECT id, version, user_id, tstamp, changeset_id, tags, nodes, bbox, linestring as geom, modo, varcosto
+  SELECT id, version, user_id, tstamp, changeset_id, tags, nodes, bbox, linestring as geom, route_type, varcosto
   FROM adb.web_000_grafo_costi 
 )
 
 
   SELECT 
-     id, version, user_id, tstamp, changeset_id, tags, nodes, bbox, geom, modo,varcosto,
+     id, version, user_id, tstamp, changeset_id, tags, nodes, bbox, geom, route_type,varcosto,
     -- campo oneway (1:yes, 0:no)
     CASE
 	WHEN w.tags -> 'oneway:bus'='no' THEN 0
@@ -160,9 +160,9 @@ CREATE TABLE adb.web_00_grafo AS (
 SELECT 
    a.id, a.way_originale, a.node_start, a.node_end, 
    b.version, b.tags, b.bus, b.oneway, b.reverse,
-    a.geom, a.modo, a.varcosto
+    a.geom, a.route_type, a.varcosto
 FROM
-  adb.t_spezza_archi a JOIN adb.t_bus_oneway b ON a.way_originale=b.id and a.modo = b.modo
+  adb.t_spezza_archi a JOIN adb.t_bus_oneway b ON a.way_originale=b.id and a.route_type = b.route_type
 ORDER BY a.way_originale);
 
 -- 7. crea uno SPATIAL INDEX sulla tabella delle grafo per accelerare le query
