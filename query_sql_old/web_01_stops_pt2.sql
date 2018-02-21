c2ab80e53f470b58364f9447588c08dafe5d47cf
leleferrara <33225900+leleferrara@users.noreply.github.com>
2018-02-21 10:28:11 +0100

    Update web_01_stops_pt2.sql

--- query_sql_old/web_01_stops_pt2.sql
+++ query_sql_old/web_01_stops_pt2.sql
@@ -6,10 +6,10 @@
 -- TABELLA OUTPUT:
 -- 1. adb.web_01_fermate_buffer
 
-
+DROP TABLE adb.web_01_fermate_buffer;
 CREATE TABLE adb.web_01_fermate_buffer AS 
 SELECT  
-  stop_id, ST_BUFFER(geom,240) geom_buff, geom 
+  stop_id, ST_TRANSFORM(ST_BUFFER(ST_TRANSFORM (geom,3003), 240),4326) geom_buff,  route_type, geom 
 FROM adb.web_01_fermate;
 
 CREATE INDEX web_01_fermate_buffer_idx ON adb.web_01_fermate_buffer USING GIST (geom)
