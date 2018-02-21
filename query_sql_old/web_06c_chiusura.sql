f606ca411c3f5b36c5706efc2c99bb7aec63c2de
leleferrara <33225900+leleferrara@users.noreply.github.com>
2018-02-21 10:53:41 +0100

    Update web_06c_chiusura.sql

--- query_sql_old/web_06c_chiusura.sql
+++ query_sql_old/web_06c_chiusura.sql
@@ -1,4 +1,6 @@
-﻿DROP TABLE adb.t_web_04_stops;
+﻿--ELIMINO LE TABELLE DI APPOGGIO UTILIZZATE PER IL ROUTING
+ 
+ DROP TABLE adb.t_web_04_stops;
 DROP TABLE adb.t_web_02_grafo_vertices_pgr;
 DROP TABLE adb.t_web_02_grafo;
 DROP TABLE adb.t_web_05_grafo;
