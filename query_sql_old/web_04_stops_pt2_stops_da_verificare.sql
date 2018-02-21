a6da3e6568bab6a1ec29b8f3630329995bdbc182
leleferrara <33225900+leleferrara@users.noreply.github.com>
2018-02-21 10:45:20 +0100

    Update web_04_stops_pt2_stops_da_verificare.sql

--- query_sql_old/web_04_stops_pt2_stops_da_verificare.sql
+++ query_sql_old/web_04_stops_pt2_stops_da_verificare.sql
@@ -10,7 +10,7 @@
 -- TABELLA OUTPUT:
 -- 1. adb.web_04_stops_da_verificare
 
-
+DROP TABLE adb.web_04_stops_da_verificare ;
 CREATE TABLE adb.web_04_stops_da_verificare AS 
 -- 1. CREA SPATIAL INDEX
 -- CREATE INDEX web_lines_idx ON adb.web_lines USING GIST (geom);
@@ -18,7 +18,7 @@ CREATE TABLE adb.web_04_stops_da_verificare AS
 -- 2. seleziona i record sui quali bisogna fare la procedura delle STOPS 
 WITH stops AS (
   SELECT 
-    stop_id_s, stop_id_e, stop_code_s, stop_code_e, line_id, line_code, geom, geom_check
+    route_type, stop_id_s, stop_id_e, stop_code_s, stop_code_e, line_id, line_code, geom, geom_check
   FROM 
     adb.web_lines
 ),
@@ -31,7 +31,8 @@ stops_start AS (
     stops a,
     adb.web_03_fermate_nodo b
   WHERE 
-    a.stop_id_s=ANY(stop_id_originali)
+    a.stop_id_s=ANY(stop_id_originali) 
+    AND a.route_type = b.route_type
 ),
 
 -- 3.2.
@@ -44,6 +45,7 @@ stops_end AS (
     adb.web_03_fermate_nodo b
   WHERE 
     a.stop_id_e=ANY(stop_id_originali)
+    AND a.route_type = b.route_type
 ),
 
 -----------------------------------------------------------------------------------------------------------------
@@ -54,7 +56,7 @@ stops_end AS (
 -- 4.1. seleziona i record sui quali bisogna fare la procedura delle LINES 
 lines AS (
   SELECT 
-    stop_id_s, stop_id_e, stop_code_s, stop_code_e, line_id, line_code, 
+    route_type, stop_id_s, stop_id_e, stop_code_s, stop_code_e, line_id, line_code, 
     geom_check, nodo_partenza, nodo_arrivo, contatore, ST_TRANSFORM(geom, 3003) AS geom 
   FROM 
     stops_end
@@ -65,7 +67,7 @@ lines AS (
 -- 4.2. CREATE A BUFFER AROUND THE LINES
 lines_buffer AS(
   SELECT 
-    stop_id_s, stop_id_e, stop_code_s, stop_code_e, line_id, line_code, contatore, ST_BUFFER(geom,200) AS geom, geom_check
+    route_type, stop_id_s, stop_id_e, stop_code_s, stop_code_e, line_id, line_code, contatore, ST_BUFFER(geom,200) AS geom, geom_check
   FROM 
     lines
 ),
@@ -73,28 +75,29 @@ lines_buffer AS(
 -- 4.3. VERIFICA
 verifica AS (
   SELECT 
-    stop_id_s, stop_id_e, contatore, SUM(prob_start) prob_start, SUM(prob_end) prob_end 
+    route_type, stop_id_s, stop_id_e, contatore, SUM(prob_start) prob_start, SUM(prob_end) prob_end 
   FROM
-    (SELECT stop_id_s, stop_id_e, contatore, 1 prob_start, 0 prob_end 
+    (SELECT a.route_type, stop_id_s, stop_id_e, contatore, 1 prob_start, 0 prob_end 
      FROM 
        lines_buffer a,
        adb.web_03_fermate_nodo b
      WHERE
        NOT ST_INTERSECTS(a.geom, b.geom) AND
        stop_id_s=ANY(stop_id_originali)
-       
+       AND a.route_type = b.route_type
     UNION
 
-     SELECT stop_id_s, stop_id_e, contatore, 0 prob_start, 1 prob_end
+     SELECT a.route_type, stop_id_s, stop_id_e, contatore, 0 prob_start, 1 prob_end
      FROM 
        lines_buffer a,
        adb.web_03_fermate_nodo b
      WHERE
        NOT ST_INTERSECTS(a.geom, b.geom) AND
        stop_id_e=ANY(stop_id_originali)
+       AND a.route_type = b.route_type
     )a
   GROUP BY
-    stop_id_s, stop_id_e, contatore
+    route_type, stop_id_s, stop_id_e, contatore
   ORDER BY contatore
 )
 
@@ -120,4 +123,5 @@ SELECT stop_id_s, stop_id_e, stop_code_s, stop_code_e, line_id, line_code,
   LEFT JOIN st b ON stop_id_e = b.stop_id
   order by e_d desc
   */
+
 
