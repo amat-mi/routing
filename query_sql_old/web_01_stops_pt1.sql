040acfb14ad31fdbc42c138a30fa323e4cae5ce1
leleferrara <33225900+leleferrara@users.noreply.github.com>
2018-02-21 10:26:46 +0100

    Update web_01_stops_pt1.sql

--- query_sql_old/web_01_stops_pt1.sql
+++ query_sql_old/web_01_stops_pt1.sql
@@ -1,5 +1,6 @@
-﻿--1. GROUP STOPS BY GEOMETRY AND TRANSFORM STOPS FROM 4326 TO 3003 
+--1. GROUP STOPS BY GEOMETRY AND TRANSFORM STOPS FROM 4326 TO 3003 
 --   raggruppare le fermate serve per spezzare il grafo. 
+--   ad ogni fermata viene associato il modo di trasporto
 
 -- TABELLE INPUT:
 -- 1. adb.web_stops
@@ -7,12 +8,16 @@
 -- TABELLA OUTPUT:
 -- 1. adb.web_01_fermate
 
-
+DROP TABLE adb.web_01_fermate;
 CREATE TABLE adb.web_01_fermate AS 
+
+--verifica dei route_type presenti in db
+
   SELECT 
-    ROW_NUMBER() OVER(ORDER BY geom) AS stop_id, ARRAY_AGG(stop_id) stop_id_originali, ST_TRANSFORM(geom, 3003) geom
+    ROW_NUMBER() OVER(ORDER BY geom) AS stop_id, route_type, ARRAY_AGG(stop_id) stop_id_originali,  geom
   FROM adb.web_stops
-  GROUP BY geom;
+  GROUP BY geom, route_type;
 
 -- 2. crea uno SPATIAL INDEX sulla tabella delle fermate per accelerare le query
-CREATE INDEX web_01_fermate_idx ON adb.web_01_fermate USING GIST (geom);  
+
+CREATE INDEX web_01_fermate_idx ON adb.web_01_fermate USING GIST (geom);
