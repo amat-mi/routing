0b3f0e8d8a7d87c1f1491763877ffc51c8867e0f
leleferrara <33225900+leleferrara@users.noreply.github.com>
2018-02-21 10:12:42 +0100

    Importazione in adb
    esempio di specifiche per importare i file estratti al punto precedente all'interno del database MONITORAGGIO_TPL

---
+++ query_sql_old/web_000000000_importa_osm [MONITORAGGIO_TPL].sql
@@ -0,0 +1,10 @@
+﻿-- IMPORTAZIONE
+
+DELETE FROM adb.cart_osm_ways;
+DELETE FROM adb.cart_osm_way_nodes;
+DELETE FROM  adb.cart_osm_nodes;
+
+
+COPY adb.cart_osm_ways
+FROM E'/areascambio/osm/ways.csv'
+with delimiter as ';' csv header
