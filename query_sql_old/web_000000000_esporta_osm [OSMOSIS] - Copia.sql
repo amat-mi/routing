1e72f978cb597159a8b355ab13413143369cf644
francot <frenk.calza@gmail.com>
2018-03-08 13:08:36 +0100

    Update web_000000000_esporta_osm [OSMOSIS] - Copia.sql

--- query_sql_old/web_000000000_esporta_osm [OSMOSIS] - Copia.sql
+++ query_sql_old/web_000000000_esporta_osm [OSMOSIS] - Copia.sql
@@ -1,21 +1,35 @@
-﻿-- non eseguirlo come un unico script, ma i vari sub-script separatamente
+--- LE SEGUENTI QUERY LAVORANO SU DB osmosis
 
 
--- ESPORTAZIONE I TRE SELECT SEPARATAMENTE COME TESTO : 
+
+-- non eseguirlo come un unico script, ma i vari sub-script separatamente
+
+
+-- ESPORTARE I TRE SELECT SEPARATAMENTE COME TESTO : 
 
 
 --GRAFO
+---- seleziona tutti gli archi con geometria valida e non nulla
+
 SELECT id, tags, nodes, bbox, linestring
   FROM public.ways
-  WHERE (tags::text LIKE '%highway%' or tags::text LIKE '%railway%' )
+---- aggiungere archi pedonali per PTAL
+WHERE (tags::text LIKE '%highway%' or tags::text LIKE '%railway%' )
+--------------------
 AND  LINESTRING is not null
   AND  st_isvalid(LINESTRING);
 
+
+
 --WAY_NODES
+----  topologia dell' arco espressa come sequenza di nodi
+
 WITH ID_VALIDI AS (
 	SELECT *
 	  FROM public.ways
+	---- aggiungere archi pedonali per PTAL
 	  WHERE (tags::text LIKE '%highway%' or tags::text LIKE '%railway%' )
+	-------
 	AND  LINESTRING is not null
 	  AND  st_isvalid(LINESTRING))
 
@@ -23,10 +37,11 @@ SELECT * FROM public.way_nodes
 	JOIN ID_VALIDI USING (way_id);
 
 --NODES
-
+--- nodi con geometria
 WITH NODI_VALIDI AS (
 	SELECT DISTINCT unnest(nodes) AS id
 	  FROM public.ways
+	---- aggiungere archi pedonali per PTAL
 	  WHERE (tags::text LIKE '%highway%' or tags::text LIKE '%railway%' )
 	AND  LINESTRING is not null
 	  AND  st_isvalid(LINESTRING))
